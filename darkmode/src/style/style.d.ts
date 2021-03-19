7a2b086b71066e1c18ef348e68c22fed9088952d
dohye1 <tsc03018@gmail.com>
2021-03-19 13:09:42 +0900

    style: style의 타입 설정 및 light, dark 모드 시 적용할 색상 정리

---
+++ darkmode/src/style/style.d.ts
@@ -0,0 +1,22 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    mode: {
+      mainBackground: string
+      primaryText: string
+      secondaryText: string
+      disable: string
+      border: string
+      divider: string
+      background: string
+      tableHeader: string
+      themeIcon: string
+      blue1: string
+      blue2: string
+      blue3: string
+      green: string
+      gray: string
+    }
+  }
+}
